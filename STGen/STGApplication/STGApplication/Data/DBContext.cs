c641a7553a030c7aebf234e7be97e89165ad7d76
HFOXH <oscv200@gmail.com>
2023-09-29 11:47:54 -0500

    Creacion con scaffolding

---
+++ STGen/STGApplication/STGApplication/Data/DBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using STGApplication.Models;
+
+namespace STGApplication.Data
+{
+    public class DBContext: DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Animal> animal { get; set; }
+    }
+}
---
+++ STGen/STGApplication/STGApplication/Models/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace STGApplication.Models
+{
+    public class Animal
+    {
+        [Key]
+        [Required]
+        public int AnimalId { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Breed { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string Sex { get; set; }
+        [Required]
+        public Decimal Price { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string Status { get; set; }
+    }
+}
